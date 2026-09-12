1196e70f02c27662017a62a8af7ff36275361ec2
yazeedx91 <yazeedx91@gmail.com>
2026-09-12 23:11:39 +0300

    infra: explicitly enable Key Vault RBAC

--- infra/production/terraform/vault.tf
+++ infra/production/terraform/vault.tf
@@ -18,6 +18,7 @@ resource "azurerm_key_vault" "projectx" {
   sku_name                      = "standard"
   soft_delete_retention_days    = 90
   purge_protection_enabled      = true
+  rbac_authorization_enabled    = true
   public_network_access_enabled = false
 }
 
